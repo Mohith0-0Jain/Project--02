4faecc0b9e2a02475c28434102be1ac7e47d1997
Mohith Jain <mj9299395@gmail.com>
2025-05-08 12:41:39 +0530

    Add Initial file

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV != "production"){
+if (process.env.NODE_ENV != "production") {
     require("dotenv").config();
 }
 
@@ -8,7 +8,7 @@ const app = express();
 
 
 
-const dbUrl =process.env.ATLASDB_URL;
+const dbUrl = process.env.ATLASDB_URL;
 
 const path = require("path");
 const methodOverride = require("method-override");
@@ -33,41 +33,40 @@ main().then(() => {
 });
 
 //Creating a database 
-async function main () {
+async function main() {
     await mongoose.connect(dbUrl);
 }
 //ejs templating
 
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
-app.use(express.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
-app.use(express.static(path.join(__dirname,"/public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 const store = MongoStore.create({
-    mongoUrl:dbUrl,
+    mongoUrl: dbUrl,
     crypto: {
-        secret : process.env.SECRET,
+        secret: process.env.SECRET,
     },
-    touchAfter : 24 *3600,
-    
+    touchAfter: 24 * 3600,
+
 });
-store.on("error",() => {
-    console.log("ERROR in mongo session store",err);
+store.on("error", (err) => {
+    console.log("ERROR in mongo session store", err);
 });
 
 const sessionOptions = {
     store,
-    secret : process.env.SECRET,
-    resave : false,
-    saveUninitialized : true,
-    cookie : {
-        expires : Date.now() + 7*24*60 *60*1000,
-        maxAge :  7*24*60 *60*1000,
-        httpOnly : true,
-    }, 
-    
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+    },
 };
 
 
@@ -75,47 +74,46 @@ app.use(session(sessionOptions));
 app.use(flash());
 
 app.use(passport.initialize());
-app.use(passport.session()); 
+app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 // use static serialize and deserialize of model for passport session support
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req,res,next) => {
+app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     res.locals.currUser = req.user;
-    
     next();
 });
 
-//demo useer
+// demo user
 // app.get("/demouser", async (req , res ) => {
 //     let fakeUser = new User({
 //         email :"student@gmail.com",
-//         username :"Harsh"
+//         username :"mohith"
 //     });
-//     let registerdUser=  await User.register(fakeUser, "helloworld");
+//     let registerdUser=  await User.register(fakeUser, "hello");
 //     res.send(registerdUser);
 // });
 
 
 //Restructuirng listings 
-app.use("/listings",listingRouter);
+app.use("/listings", listingRouter);
 //Restructuring reviews
-app.use("/listings/:id/reviews" ,reviewRouter);
-app.use("/",userRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
-app.all("*", (req ,res , next) => {
+app.all("*", (req, res, next) => {
     next(new ExpressError(404, "Page not found!"));
 });
 
-app.use((err, req,res,next) => {
-    let {statusCode = 500 , message ="Something went wrong"}= err;
-    res.status(statusCode).render("error.ejs",{message});
+app.use((err, req, res, next) => {
+    let { statusCode = 500, message = "Something went wrong" } = err;
+    res.status(statusCode).render("error.ejs", { message });
     // res.status(statusCode).send(message);
 });
 
-app.listen(  8080 , () => {
+app.listen(8080, () => {
     console.log("server is listening to port 8080");
 });
--- routes/user.js
+++ routes/user.js
@@ -3,27 +3,28 @@ const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
-const {saveRedirectUrl} = require("../middleware.js");
+const { saveRedirectUrl } = require("../middleware.js");
 const { signUp } = require("../controllers/users.js");
 const userController = require("../controllers/users.js");
 
 router
-.route("/signup")
-.get(userController.renderSignUpForm)
-.post(wrapAsync(userController.signup));
+    .route("/signup")
+    .get(userController.renderSignUpForm)
+    .post(wrapAsync(userController.signup));
 
-router 
-.route("/login")
-.get(userController.renderLoginForm)
-.post(
-     saveRedirectUrl,
-     passport.authenticate("local", {
-    failureRedirect:'/login',
-    failureFlash:true}), 
-    userController.login,
-);
+router
+    .route("/login")
+    .get(userController.renderLoginForm)
+    .post(
+        saveRedirectUrl,
+        passport.authenticate("local", {
+            failureRedirect: '/login',
+            failureFlash: true
+        }),
+        userController.login,
+    );
 
 //Logout
 
-router.get("/logout",userController.logout);
-module.exports =router;
+router.get("/logout", userController.logout);
+module.exports = router;
